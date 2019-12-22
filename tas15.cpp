7d7bbfa8cbe8e81b9acabb6ed3b477949ed2d9be
Vladislav <vladhaluev@yandex.ru>
2019-12-22 20:03:57 +0300

    task15 part3

--- tas15.cpp
+++ tas15.cpp
@@ -63,16 +63,60 @@ Graph lab15(Graph gr,QVector<QString> v1,QVector<QString> v2){
 
 
 
-    while(vec.size()>0){
+    while(vec.size()>1 ){
+        qDebug()<<"1";
         for(auto v : all_vertex)
-        {
+        {qDebug()<<"2";
             if(vec.contains(v->getName())){
                 if(gr.vertexAt(v).size()==1)
-                {
+                {qDebug()<<"3";
                     for (auto edge : gr.vertexAt(v))
-                    {
+                    {qDebug()<<"4";
+                        qDebug()<<v->getName();
+                        qDebug()<<edge->getVertex()->getName();
                         vertex_smpt v1(new Vertex(v->getName(), (1+k)%200+ rand()%200,(1+k)%200+rand()%200));
-                        v1->setColor(Qt::black);
+                        v1->setColor(Qt::red);
+                        graph.insert(v1, QList<edge_smpt>());
+                        vertex_smpt v2(new Vertex(edge->getVertex()->getName(), (2+k)%200+ rand()%200,(2+k)%200+rand()%200));
+                        v2->setColor(Qt::magenta);
+                        graph.insert(v2, QList<edge_smpt>());
+                        edge_smpt edge1 = edge_smpt(new Edge(v1, v2, 1));
+                        graph[v1].append(edge);
+                        qDebug()<<vec;
+                        vec.erase(std::remove(vec.begin(), vec.end(), v->getName()), vec.end());
+                        vec.erase(std::remove(vec.begin(), vec.end(), edge->getVertex()->getName()), vec.end());
+                        qDebug()<<vec;
+                    }
+                }
+            }
+        }
+        qDebug()<<"5";
+        QVector<int> sizeVec;
+        for(auto v : all_vertex)
+        {   if(vec.contains(v->getName()) &&gr.vertexAt(v).size()!=0)
+                sizeVec.append(gr.vertexAt(v).size());
+        }
+        if(sizeVec.size()==0)
+            break;
+        qDebug()<<"6";
+        bool stop = false;
+        qDebug()<<sizeVec;
+        qSort(sizeVec);
+        qDebug()<<sizeVec;
+        qDebug()<<"7";
+        if(vec.size()>0){
+            qDebug()<<"8";
+        for(auto v:all_vertex){
+            qDebug()<<"9";
+            if(vec.contains(v->getName())){
+                qDebug()<<"10";
+                if(sizeVec.size()>0){
+                if(gr.vertexAt(v).size()==sizeVec.last())
+                {qDebug()<<"11";
+                    for (auto edge : gr.vertexAt(v))
+                    {qDebug()<<"12";
+                        vertex_smpt v1(new Vertex(v->getName(), (1+k)%200+ rand()%200,(1+k)%200+rand()%200));
+                        v1->setColor(Qt::red);
                         graph.insert(v1, QList<edge_smpt>());
                         vertex_smpt v2(new Vertex(edge->getVertex()->getName(), (2+k)%200+ rand()%200,(2+k)%200+rand()%200));
                         v2->setColor(Qt::magenta);
@@ -81,17 +125,19 @@ Graph lab15(Graph gr,QVector<QString> v1,QVector<QString> v2){
                         graph[v1].append(edge);
                         vec.erase(std::remove(vec.begin(), vec.end(), v->getName()), vec.end());
                         vec.erase(std::remove(vec.begin(), vec.end(), edge->getVertex()->getName()), vec.end());
+                        stop = true;
+                        break;
+                        qDebug()<<"here31";
                     }
                 }
+                }
             }
+            if(stop)
+                break;
         }
-        QVector<int> sizeVec;
-        QList<int>vec;
-        for(auto v : all_vertex)
-        {
-            sizeVec.append(gr.vertexAt(v).size());
         }
-        qSort(sizeVec);
+        qDebug()<<"inWhile";
+
     }
 
 
